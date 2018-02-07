221aa3bf63b12c6b4f0bbe8543d6b70ce91dd804
Katherine Manning <katm201@gmail.com>
2018-02-06 20:17:15 -0800

    (feat) add outline for Board and CreateGame containers

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import Board from '../containers/Board';
+import CreateGame from '../containers/CreateGame';
+
 const Main = () => (
   <div className="main">
     Main Component
+    <Board className="board-wrapper" />
+    <CreateGame className="create-game-wrapper" />
   </div>
 );
 
