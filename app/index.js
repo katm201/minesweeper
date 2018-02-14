d503cfcf39a39cda5ea258427ad10209b1403ef7
Katherine Manning <katm201@gmail.com>
2018-02-14 11:57:47 -0800

    (setup) add redux store to app

--- app/index.js
+++ app/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers/index';
 import './public/assets/stylesheet.css';
 
-render(<App />, document.getElementById('root'));
+render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
