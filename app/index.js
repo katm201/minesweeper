f01ed8a76ac2c3cf9f818e0aad9b25b807b4ab8f
Katherine Manning <katm201@gmail.com>
2018-02-14 11:59:27 -0800

    (ref) add missing comma and rename reducers function

--- app/index.js
+++ app/index.js
@@ -4,11 +4,11 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import App from './components/App';
-import reducers from './reducers/index';
+import rootReducer from './reducers/index';
 import './public/assets/stylesheet.css';
 
 render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(rootReducer)}>
     <App />
   </Provider>,
   document.getElementById('root'),
